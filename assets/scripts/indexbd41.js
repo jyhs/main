e0942bd5b7367296485fd291f8e768496669cab9
Coral <coral@CoraldeiMac.local>
2018-05-26 13:41:31 +0800

    update

--- assets/scripts/indexbd41.js
+++ assets/scripts/indexbd41.js
@@ -1,3 +1,20 @@
+var browser = {
+    versions:function(){ 
+        var u = navigator.userAgent, app = navigator.appVersion; 
+        return {
+           trident: u.indexOf('Trident') > -1, //IE内核
+           presto: u.indexOf('Presto') > -1, //opera内核
+           webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+           gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+           mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+           ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+           android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+           iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone
+           iPad: u.indexOf('iPad') > -1, //是否iPad
+        };
+    }(),
+    language:(navigator.browserLanguage || navigator.language).toLowerCase()
+};
 webpackJsonp([0], {
 	11: function(e, t, n) {
 		"use strict";
@@ -16,7 +33,11 @@ webpackJsonp([0], {
 			height: "100%"
         });
         qr.addEventListener("click", function(e) {
-			e.preventDefault && e.preventDefault(), e.stopPropagation && e.stopPropagation(), qrdiv.style.display = "block"
+			if(browser.versions.mobile){
+				window.location.href='http://mobile.coral123.com';
+			}else{
+				e.preventDefault && e.preventDefault(), e.stopPropagation && e.stopPropagation(), qrdiv.style.display = "block"
+			}
         }),
         qrClose.addEventListener("click", function(e) {
 			e.preventDefault && e.preventDefault(), e.stopPropagation && e.stopPropagation(), qrdiv.style.display = "none"
